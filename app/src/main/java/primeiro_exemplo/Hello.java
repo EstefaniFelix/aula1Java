bba3c252e564edc307b3650e7ef5e3abc8556c40
EstefaniFelix <estefaniescola1@gmail.com>
2024-02-09 15:11:06 +0000

    save

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -2,6 +2,10 @@ package primeiro_exemplo;
 
 public class Hello {
     public static void main(String[] args) {
+        int cont = 10;
+        String nome = "Estefani";
         System.out.println("Olá mundo!!!");
+
+        System.out.println(cont + " : " + nome);
     }
 }
